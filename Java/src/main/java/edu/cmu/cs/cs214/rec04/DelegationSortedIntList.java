2e083a30db77e214583ad5336fba3d141dfc3dd5
Nyamod <nyamsurennyamod9@gmail.com>
2026-03-16 00:08:04 +0800

    Лаборатор 6: Удамшил ба Бүрдмэл класс даалгавар амжилттай

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -1,16 +1,44 @@
 package edu.cmu.cs.cs214.rec04;
 
 /**
- * DelegationSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
- *
- * @author Nora Shoemaker
- *
+ * Delegation (Бүрдмэл) аргаар хэрэгжүүлсэн класс.
+ * IntegerList интерфэйсийг хэрэгжүүлж, үйлдлийг дотоод объект руу дамжуулна[cite: 26, 27].
  */
+public class DelegationSortedIntList implements IntegerList {
+    
+    // Жагсаалтыг хадгалах функциональ байдлыг дамжуулах дотоод объект [cite: 27, 28]
+    private SortedIntList list = new SortedIntList();
+    private int totalAdded = 0;
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+    @Override
+    public boolean add(int element) {
+        totalAdded++; // Нэмэх оролдлого бүрийг тоолно [cite: 15, 29]
+        return list.add(element); // Үйлдлийг дотоод объект руу дамжуулна [cite: 28]
+    }
 
+    @Override
+    public boolean addAll(IntegerList elements) {
+        totalAdded += elements.size(); // Жагсаалтын хэмжээгээр тоолуурыг нэмнэ [cite: 15, 29]
+        return list.addAll(elements); // Үйлдлийг дамжуулна [cite: 28]
+    }
+
+    // Бусад IntegerList интерфэйсийн аргуудыг шууд дамжуулах (Delegation) [cite: 28]
+    @Override
+    public int get(int index) { return list.get(index); }
+
+    @Override
+    public boolean remove(int element) { return list.remove(element); }
+
+    @Override
+    public boolean removeAll(IntegerList elements) { return list.removeAll(elements); }
+
+    @Override
+    public int size() { return list.size(); }
+
+    /**
+     * Нийт нэмэгдсэн элементийн тоог авах функц[cite: 15, 29].
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,16 +1,43 @@
+
+
 package edu.cmu.cs.cs214.rec04;
 
+
 /**
- * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed)
- * and exports an accessor (totalAdded) for this count.
- *
- * @author Nora Shoemaker
- *
+ * SortedIntList-ийг удамшуулж, нийт нэмэгдсэн элементийн тоог
+ * хянадаг дэд класс. [cite: 7, 13]
  */
+public class InheritanceSortedIntList extends SortedIntList {
+    
+    // Жагсаалт руу нэмэгдсэн нийт элементийн тоог хадгалах хувьсагч [cite: 17]
+    private int totalAdded = 0;
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+    /**
+     * Ганц элемент нэмэх функцийг override хийх [cite: 20, 21]
+     */
+    @Override
+    public boolean add(int element) {
+        // Нэмэх оролдлого хийх бүрт тоолуурыг нэмэгдүүлнэ [cite: 15]
+        totalAdded++;
+        // Эцэг классын (SortedIntList) үндсэн add үйлдлийг дуудна [cite: 16]
+        return super.add(element);
+    }
 
+    /**
+     * Олон элементийг нэгэн зэрэг нэмэх функцийг override хийх [cite: 20, 21]
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        // Орж ирсэн жагсаалтын хэмжээгээр тоолуурыг нэмэгдүүлнэ [cite: 15, 22]
+        totalAdded += list.size();
+        // Эцэг классын addAll үйлдлийг дуудна [cite: 16]
+        return super.addAll(list);
+    }
+
+    /**
+     * Үүсгэснээс хойш нийт нэмэгдсэн элементийн тоог буцаах (access method) [cite: 15]
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
